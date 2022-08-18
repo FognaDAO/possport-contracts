9bd330b65720b1a40453d263145585db0df5ee4e
Epifanio Ferrari <ferrariepifanio@gmail.com>
2022-08-19 01:28:02 +0200

    Updated tokens metadata

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -28,7 +28,7 @@ def test_upgrade(deployment):
     assert token.newValue() == new_value
     assert token.baseURI() == "ipfs://"
     assert not token.baseEndURI()
-    assert token.tokenURI(1) == "QmPnKZpXa7ypDWp8XEfsnZbCzhS5JC98S48pjtGXdb6Jb3"
+    assert token.tokenURI(1) == "bafkreibbhwpf3wwooe7x2zz555ihmolbhtoq4pbuxsfuhi65z5zt4ibcnu"
     assert token.openseaProxy() == "0x58807baD0B376efc12F5AD86aAc70E78ed67deaE"
     assert token.balanceOf(accounts[0]) == 50
     tokenId = 1
