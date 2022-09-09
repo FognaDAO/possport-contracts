dd2a56920fc7ddadfb428f47b670d9135019e706
Epifanio Ferrari <ferrariepifanio@gmail.com>
2022-09-10 00:54:40 +0200

    Added solc version in brownie-config.yaml and removed file install_compiler.py

--- install_compiler.py
+++
@@ -1,8 +0,0 @@
-# Note: this script is called during docker build to
-# pre-download solc at Docker image build time, so it
-# will be immediately available when the container starts.
-
-from brownie.project.compiler import install_solc
-
-if __name__ == "__main__":
-    install_solc("0.8.12")
